da47cec4b7c88c9e9e31b1e49f6abdf09fd342a2
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-11-03 18:41:41 +0100

    [FEAT] Added initial project navigation and navbar

---
+++ Eventy/app/src/main/java/com/example/eventy/login/LoginFragment.java
@@ -0,0 +1,31 @@
+package com.example.eventy.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.eventy.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ Eventy/app/src/main/java/com/example/eventy/register/RegisterFragment.java
@@ -0,0 +1,30 @@
+package com.example.eventy.register;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.eventy.databinding.FragmentRegisterBinding;
+
+public class RegisterFragment extends Fragment {
+
+    private FragmentRegisterBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
