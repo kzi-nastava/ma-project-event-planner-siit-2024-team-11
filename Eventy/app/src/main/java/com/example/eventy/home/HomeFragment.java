b08b94da2e83d8a048bffe47145ba6a935a14636
Tamara Cvjetkovic <c.tamara02@gmail.com>
2024-11-08 17:59:13 +0100

    [ADD] tab buttons (/home)

--- Eventy/app/src/main/java/com/example/eventy/home/HomeFragment.java
+++ Eventy/app/src/main/java/com/example/eventy/home/HomeFragment.java
@@ -6,9 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.eventy.R;
 import com.example.eventy.databinding.FragmentHomeBinding;
+
 
 public class HomeFragment extends Fragment {
 
@@ -19,6 +22,22 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        binding.tabEvent.setOnClickListener(v -> {
+            binding.tabEvent.setTextColor(ContextCompat.getColor(v.getContext(), R.color.tab_active_text_color));
+            binding.tabEvent.setBackground(ContextCompat.getDrawable(v.getContext(), R.drawable.tab_active_background));
+
+            binding.tabSolutions.setTextColor(ContextCompat.getColor(v.getContext(), R.color.tab_inactive_text_color));
+            binding.tabSolutions.setBackground(ContextCompat.getDrawable(v.getContext(), R.drawable.tab_inactive_background));
+        });
+
+        binding.tabSolutions.setOnClickListener(v -> {
+            binding.tabEvent.setTextColor(ContextCompat.getColor(v.getContext(), R.color.tab_inactive_text_color));
+            binding.tabEvent.setBackground(ContextCompat.getDrawable(v.getContext(), R.drawable.tab_inactive_background));
+
+            binding.tabSolutions.setTextColor(ContextCompat.getColor(v.getContext(), R.color.tab_active_text_color));
+            binding.tabSolutions.setBackground(ContextCompat.getDrawable(v.getContext(), R.drawable.tab_active_background));
+        });
 
         return root;
     }
