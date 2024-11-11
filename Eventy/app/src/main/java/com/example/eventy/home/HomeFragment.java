d450bbbfabaa2b4ffc6cd8f4f2eb9d077672166b
Tamara Cvjetkovic <c.tamara02@gmail.com>
2024-11-11 19:32:40 +0100

    [ADD] initial filter & search implementation (needs to be changed)

--- Eventy/app/src/main/java/com/example/eventy/home/HomeFragment.java
+++ Eventy/app/src/main/java/com/example/eventy/home/HomeFragment.java
@@ -1,23 +1,39 @@
 package com.example.eventy.home;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.eventy.R;
 import com.example.eventy.databinding.FragmentHomeBinding;
 import com.example.eventy.home.events.EventsFragment;
+import com.example.eventy.home.events.EventsViewModel;
 import com.example.eventy.home.events.featured_events.FeaturedEventsFragment;
 import com.example.eventy.home.events.featured_events.FeaturedEventsTitleFragment;
 import com.example.eventy.register.RegisterOrganiserFragment;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
+    private EventsViewModel eventsViewModel;
+    private boolean isFirstSelection = true;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -79,6 +95,73 @@ public class HomeFragment extends Fragment {
                     .replace(R.id.tab_view, fragment)
                     .addToBackStack(null)
                     .commit();
+        });
+
+        eventsViewModel = new ViewModelProvider(this).get(EventsViewModel.class);
+        SearchView searchView = binding.searchInput;
+        /* Posmatranje LiveData objekta i ažuriranje UI-a:
+         * Ovaj deo koda dodaje observer na LiveData<String> objekat unutar
+         * ProductsPageViewModel. Svaki put kada dođe do promene podatka
+         * unutar LiveData objekta (u ovom slučaju searchText),
+         * ta promena se automatski prosleđuje i aktivira se metoda
+         * setQueryHint na SearchView komponenti sa novom vrednošću.
+         * Funkcija getViewLifecycleOwner() garantuje da se observer
+         * povezuje sa životnim ciklusom vlasnika prikaza fragmenta,
+         * što znači da će se observer automatski ukloniti kada fragment
+         * više nije vidljiv ili je uništen, sprečavajući time moguće curenje memorije.
+         * */
+        eventsViewModel.getText().observe(getViewLifecycleOwner(), searchView::setQueryHint);
+
+        Button filterButton = binding.filterButton;
+        filterButton.setOnClickListener(v -> {
+            Toast.makeText(this.getContext(), "Filter button clicked!", Toast.LENGTH_SHORT).show();
+
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
+            View dialogView = getLayoutInflater().inflate(R.layout.bottom_sheet_event_filter, null);
+            bottomSheetDialog.setContentView(dialogView);
+            bottomSheetDialog.show();
+        });
+
+        Spinner spinner = binding.sortButton;
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.event_sort_options));
+        // Specify the layout to use when the list of choices appears
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Provera da li je ovo prvi poziv
+                if (isFirstSelection) {
+                    isFirstSelection = false;
+                    return; // Izlazak iz metode bez prikazivanja dijaloga
+                }
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+                dialog.setMessage("Change the sort option?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.v("ShopApp", (String) parent.getItemAtPosition(position));
+                                ((TextView) parent.getChildAt(0)).setTextColor(Color.MAGENTA);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = dialog.create();
+                alert.show();
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
         });
 
         return root;
