43748603e28d0d17c3cc9a6f0087487ebd91c282
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-11-24 18:00:15 +0100

    [FEAT] Add Event Type fragment

--- Eventy/app/src/main/java/com/example/eventy/events/AddEventTypeFragment.java
+++ Eventy/app/src/main/java/com/example/eventy/events/AddEventTypeFragment.java
@@ -2,65 +2,73 @@ package com.example.eventy.events;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.eventy.R;
+import com.example.eventy.databinding.FragmentAddEventTypeBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddEventTypeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AddEventTypeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentAddEventTypeBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
 
-    public AddEventTypeFragment() {
-        // Required empty public constructor
-    }
+        binding = FragmentAddEventTypeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddEventTypeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddEventTypeFragment newInstance(String param1, String param2) {
-        AddEventTypeFragment fragment = new AddEventTypeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        binding.backButton.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(v);
+
+            // Problem with back button so we clear the backstack
+            navController.popBackStack();
+
+            navController.navigate(R.id.nav_event_types);
+        });
+
+        TextInputEditText multiSelectEditText = binding.selectCategoriesInput;
+        String[] options = {"Option 1", "Option 2", "Option 3", "Option 4"};
+        boolean[] selectedOptions = new boolean[options.length];
+
+        multiSelectEditText.setOnClickListener(v -> {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Select Recommended Categories")
+                    .setMultiChoiceItems(options, selectedOptions, (dialog, which, isChecked) -> {
+                        selectedOptions[which] = isChecked; // Update selected options
+                    })
+                    .setPositiveButton("OK", (dialog, which) -> {
+                        // Collect selected options
+                        StringBuilder selected = new StringBuilder();
+                        for (int i = 0; i < options.length; i++) {
+                            if (selectedOptions[i]) {
+                                if (selected.length() > 0) {
+                                    selected.append(", ");
+                                }
+                                selected.append(options[i]);
+                            }
+                        }
+                        // Display selected options
+                        multiSelectEditText.setText(selected.toString());
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+        });
+
+        return root;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_event_type, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
