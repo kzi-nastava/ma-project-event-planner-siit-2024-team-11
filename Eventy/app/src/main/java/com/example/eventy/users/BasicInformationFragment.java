ccae4d78476b2b17f135cb4aa6590b65eb639455
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-12-02 15:52:07 +0100

    [FEAT] Images added to the profile page

--- Eventy/app/src/main/java/com/example/eventy/users/BasicInformationFragment.java
+++ Eventy/app/src/main/java/com/example/eventy/users/BasicInformationFragment.java
@@ -1,22 +1,34 @@
 package com.example.eventy.users;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.eventy.R;
 import com.example.eventy.databinding.FragmentBasicInformationBinding;
+import com.example.eventy.register.CarouselAdapter;
 import com.example.eventy.users.model.User;
 import com.example.eventy.users.model.UserType;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BasicInformationFragment extends Fragment {
 
     private FragmentBasicInformationBinding binding;
     private User user;
+    private List<Uri> images = Arrays.asList(
+            Uri.parse("android.resource://com.example.eventy/" + R.mipmap.logo)
+    );
+    private CarouselAdapter carouselAdapter;
 
     public BasicInformationFragment(User user) {
         this.user = user;
@@ -38,6 +50,18 @@ public class BasicInformationFragment extends Fragment {
         binding.emailText.setText(this.user.getEmail());
         binding.addressText.setText(this.user.getAddress());
         binding.phoneNumberText.setText(this.user.getPhoneNumber());
+
+        ViewPager2 viewPager = binding.profilePictures;
+
+        if(this.user.getProfilePictures() != null && !this.user.getProfilePictures().isEmpty()) {
+            this.images = this.user.getProfilePictures()
+                    .stream()
+                    .map(Uri::parse)
+                    .collect(Collectors.toList());
+        }
+
+        carouselAdapter = new CarouselAdapter(this.images);
+        viewPager.setAdapter(carouselAdapter);
 
         return root;
     }
