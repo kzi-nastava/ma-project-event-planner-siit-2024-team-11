77f5b2ff0a82264ab3ed805c28247cb515544f17
Tamara Cvjetkovic <c.tamara02@gmail.com>
2024-12-03 03:13:54 +0100

    [ADD] create a reservation navigation (from menu)

--- Eventy/app/src/main/java/com/example/eventy/MainActivity.java
+++ Eventy/app/src/main/java/com/example/eventy/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_login, R.id.nav_register, R.id.nav_add_service, R.id.nav_edit_service,
                 R.id.nav_event_organization, R.id.nav_own_services_test, R.id.nav_event_types, R.id.nav_add_event_type,
-                R.id.nav_edit_event_type, R.id.nav_event_type_details)
+                R.id.nav_edit_event_type, R.id.nav_event_type_details, R.id.service_reservation)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
