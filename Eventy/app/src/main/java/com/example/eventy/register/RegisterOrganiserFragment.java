a496372cc8438730ed836be348705f5ae7d2cb89
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-01-05 23:14:57 +0100

    [FEAT] Register Organizer done

--- Eventy/app/src/main/java/com/example/eventy/register/RegisterOrganiserFragment.java
+++ Eventy/app/src/main/java/com/example/eventy/register/RegisterOrganiserFragment.java
@@ -1,7 +1,9 @@
 package com.example.eventy.register;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -20,15 +22,28 @@ import android.view.ViewGroup;
 
 import com.example.eventy.R;
 import com.example.eventy.databinding.FragmentRegisterOrganiserBinding;
+import com.example.eventy.users.model.AuthResponse;
+import com.example.eventy.users.model.LoginData;
+import com.example.eventy.users.model.RegisterData;
+import com.example.eventy.users.services.LoggedInHelperService;
+import com.example.eventy.utils.ClientUtils;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.BiConsumer;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterOrganiserFragment extends Fragment {
     private FragmentRegisterOrganiserBinding binding;
     private ActivityResultLauncher<Intent> galleryPickerLauncher;
+    private String profilePictureUri =
+            "/Users/rogan003/Desktop/Fakultet/5. semestar/Inzenjerstvo klijentskog sloja/Projekat/iks-project-event-planner-siit-2024-team-11/Eventy/public/ProfilePicture.png";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,6 +59,7 @@ public class RegisterOrganiserFragment extends Fragment {
                         if (data != null) {
                             Uri imageUri = data.getData();
                             binding.profilePicture.setImageURI(imageUri);
+                            this.profilePictureUri = imageUri.toString();
                         }
                     }
                 }
@@ -75,11 +91,54 @@ public class RegisterOrganiserFragment extends Fragment {
                 binding.lastNameInputLayout.getError() == null &&
                 binding.addressInputLayout.getError() == null &&
                 binding.phoneNumberInputLayout.getError() == null) {
-                NavController navController = Navigation.findNavController(v);
 
-                navController.popBackStack();
+                Call<String> call = ClientUtils.authService.register(
+                        new RegisterData(new ArrayList<String>(Arrays.asList(this.profilePictureUri)),
+                                binding.emailInput.getText().toString(),
+                                binding.passwordInput.getText().toString(),
+                                binding.confirmPasswordInput.getText().toString(),
+                                binding.firstNameInput.getText().toString(),
+                                binding.lastNameInput.getText().toString(),
+                                null,
+                                null,
+                                binding.addressInput.getText().toString(),
+                                binding.phoneNumberInput.getText().toString()));
+                call.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            new MaterialAlertDialogBuilder(requireContext())
+                                    .setTitle("Confirmation email sent")
+                                    .setMessage("Please check your email and confirm the registration!")
+                                    .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
+                                    .setIcon(R.drawable.icon_info)
+                                    .show();
 
-                navController.navigate(R.id.nav_home);
+                            NavController navController = Navigation.findNavController(v);
+
+                            navController.popBackStack();
+
+                            navController.navigate(R.id.nav_home);
+                        } else {
+                            new MaterialAlertDialogBuilder(requireContext())
+                                    .setTitle("Invalid input")
+                                    .setMessage("Invalid input data!")
+                                    .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
+                                    .setIcon(R.drawable.icon_error)
+                                    .show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        new MaterialAlertDialogBuilder(requireContext())
+                                .setTitle("Invalid input")
+                                .setMessage("Invalid input data!")
+                                .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
+                                .setIcon(R.drawable.icon_error)
+                                .show();
+                    }
+                });
             } else {
                 new MaterialAlertDialogBuilder(requireContext())
                         .setTitle("Invalid input")
