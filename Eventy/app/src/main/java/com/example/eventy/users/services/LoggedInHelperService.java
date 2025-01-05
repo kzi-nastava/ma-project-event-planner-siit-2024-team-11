232e8c7bdfe3380f278e59472408c3dfd7b9f6df
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-01-05 22:25:57 +0100

    [FEAT] Navigation is now custom according to the logged in user role (or logged out user)

--- Eventy/app/src/main/java/com/example/eventy/users/services/LoggedInHelperService.java
+++ Eventy/app/src/main/java/com/example/eventy/users/services/LoggedInHelperService.java
@@ -6,14 +6,14 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class LoggedInHelperService {
-    private static Context context;
+    private static Context appContext;
 
     public static void init(Context context) {
-        LoggedInHelperService.context = context;
+        LoggedInHelperService.appContext = context;
     }
 
     public static String getRole() {
-        String jwtToken = context.getSharedPreferences("EventyPreferences", Context.MODE_PRIVATE)
+        String jwtToken = appContext.getSharedPreferences("EventyPreferences", Context.MODE_PRIVATE)
                 .getString("JWT_TOKEN", null);
 
         if(jwtToken != null) {
@@ -26,7 +26,7 @@ public class LoggedInHelperService {
     }
 
     public static Long getId() {
-        String jwtToken = context.getSharedPreferences("EventyPreferences", Context.MODE_PRIVATE)
+        String jwtToken = appContext.getSharedPreferences("EventyPreferences", Context.MODE_PRIVATE)
                 .getString("JWT_TOKEN", null);
 
         if(jwtToken != null) {
