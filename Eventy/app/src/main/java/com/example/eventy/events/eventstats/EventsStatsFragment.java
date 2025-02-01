ed546427db58f8c7dff8510cef84fe11efee11a8
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-02-01 13:14:43 +0100

    [FEAT] Initial Event Stats layout added

--- Eventy/app/src/main/java/com/example/eventy/events/eventstats/EventsStatsFragment.java
+++ Eventy/app/src/main/java/com/example/eventy/events/eventstats/EventsStatsFragment.java
@@ -1,66 +1,207 @@
 package com.example.eventy.events.eventstats;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.example.eventy.R;
+import com.example.eventy.adapters.events.EventsAdapter;
+import com.example.eventy.common.PagedResponse;
+import com.example.eventy.custom.ErrorOkDialog;
+import com.example.eventy.databinding.FragmentEventsStatsBinding;
+import com.example.eventy.databinding.FragmentHomeEventsBinding;
+import com.example.eventy.events.model.EventCard;
+import com.example.eventy.events.model.EventFilters;
+import com.example.eventy.utils.ClientUtils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EventsStatsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class EventsStatsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentEventsStatsBinding binding;
+    private EventsAdapter eventsAdapter;
+    private int page = 0;
+    private int pageSize = 5;
+    private int totalPages = 99;
+    private String sort = "type";
+    private String search = "";
+    private EventFilters eventsFilters;
+    private ArrayList<EventCard> paginatedEvents;
+    private boolean isLoading = false;
 
     public EventsStatsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EventsStatsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EventsStatsFragment newInstance(String param1, String param2) {
-        EventsStatsFragment fragment = new EventsStatsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        eventsFilters = new EventFilters("", "-", new ArrayList<String>(), null, null, null);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentEventsStatsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.paginatedEvents = new ArrayList<>();
+        setupRecyclerView();
+        setupPaginationControls();
+        fetchEvents(search, eventsFilters, page, pageSize, sort);
+    }
+
+    private void setupRecyclerView() {
+        eventsAdapter = new EventsAdapter(requireContext(), paginatedEvents);
+        binding.eventsRecycler.setLayoutManager(new LinearLayoutManager(requireContext()));
+        binding.eventsRecycler.setAdapter(eventsAdapter);
+    }
+
+    private void setupPaginationControls() {
+        if (!isAdded()) {
+            return;
+        }
+
+        binding.btnPrevious.setOnClickListener(v -> {
+            if (page > 0) {
+                page--;
+                fetchEvents(search, eventsFilters, page, pageSize, sort);
+            }
+        });
+
+        binding.btnNext.setOnClickListener(v -> {
+            if (page < totalPages - 1) {
+                page++;
+                fetchEvents(search, eventsFilters, page, pageSize, sort);
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.page_size_options,
+                R.layout.custom_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerPageSize.setAdapter(adapter);
+        binding.spinnerPageSize.setSelection(2);
+
+        binding.spinnerPageSize.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                pageSize = Integer.parseInt(parent.getItemAtPosition(position).toString());
+                page = 0;
+                fetchEvents(search, eventsFilters, page, pageSize, sort);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // do nothing
+            }
+        });
+    }
+
+    private void fetchEvents(String search, EventFilters eventFilters, int page, int pageSize, String sort) {
+        if (!isAdded()) {
+            return;
+        }
+
+        if (isLoading) return;
+        isLoading = true;
+
+        Integer maxParticipants;
+        try {
+            maxParticipants = Integer.parseInt(eventFilters.getMaxParticipants());
+        } catch (Exception ignored) {
+            maxParticipants = null;
+        }
+        String location = eventFilters.getSelectedLocation().equals("-") ? null : eventFilters.getSelectedLocation();
+
+        Call<PagedResponse<EventCard>> call = ClientUtils.eventService.getEvents(
+                search, eventFilters.getSelectedEventTypes(), maxParticipants, location,
+                eventFilters.getSelectedStartDateTime(), eventFilters.getSelectedEndDateTime(),
+                page, pageSize, sort
+        );
+        call.enqueue(new Callback<PagedResponse<EventCard>>() {
+            @Override
+            public void onResponse(Call<PagedResponse<EventCard>> call, Response<PagedResponse<EventCard>> response) {
+                if (response.isSuccessful() && response.body() != null && getActivity() != null) {
+                    PagedResponse<EventCard> pagedResponse = response.body();
+
+                    paginatedEvents.clear();
+                    paginatedEvents.addAll(pagedResponse.getContent());
+                    eventsAdapter.notifyDataSetChanged();
+
+                    totalPages = pagedResponse.getTotalPages();
+                    updatePaginationControls();
+
+                } else {
+                    showErrorDialog("Error while loading events!");
+                    showErrorDialog(response.message());
+                }
+                isLoading = false;
+            }
+
+            @Override
+            public void onFailure(Call<PagedResponse<EventCard>> call, Throwable t) {
+                showErrorDialog("Error while loading events!");
+                showErrorDialog(t.getMessage());
+                isLoading = false;
+            }
+        });
+    }
+
+    private void updatePaginationControls() {
+        if (!isAdded()) {
+            return;
+        }
+
+        binding.btnPrevious.setEnabled(page > 0);
+        binding.btnNext.setEnabled(page < totalPages - 1);
+
+        binding.tvPageInfo.setText(String.format("Page %d of %d", page + 1, totalPages));
+    }
+
+    private void showErrorDialog(String message) {
+        if (isAdded() && getActivity() != null) {
+            ErrorOkDialog errorOkDialog = new ErrorOkDialog(getActivity(), "Error", message);
+            errorOkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            errorOkDialog.show();
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events_stats, container, false);
+    public void updateFilters(EventFilters eventFilters) {
+        if (!isAdded()) {
+            return;
+        }
+
+        this.eventsFilters = eventFilters;
+
+        page = 0;
+        pageSize = 5;
+        binding.spinnerPageSize.setSelection(2);
+
+        fetchEvents(search, eventsFilters, page, pageSize, sort);
+    }
+
+    public void updateSort(String selectedSort) {
+        sort = selectedSort;
+        fetchEvents(search, eventsFilters, page, pageSize, sort);
+    }
+
+    public void updateSearch(String searchValue) {
+        search = searchValue;
+        fetchEvents(search, eventsFilters, page, pageSize, sort);
     }
 }
