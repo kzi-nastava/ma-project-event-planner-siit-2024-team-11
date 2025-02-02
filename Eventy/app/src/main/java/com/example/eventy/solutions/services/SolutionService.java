79d9d3cd1166cb365b5c85ad093b73a465926b0c
Matijas Levang <micolevang@gmail.com>
2025-02-02 17:21:55 +0100

    [ADD] Get solution and toggle favorite solution functions

--- Eventy/app/src/main/java/com/example/eventy/solutions/services/SolutionService.java
+++ Eventy/app/src/main/java/com/example/eventy/solutions/services/SolutionService.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface SolutionService {
@@ -39,4 +41,10 @@ public interface SolutionService {
 
     @GET(prefix + "/companies")
     Call<String[]> getAllUniqueCompaniesForSolutions();
+
+    @GET(prefix + "/cards/{solutionId}")
+    Call<SolutionCard> getSolution(@Path("solutionId") Long solutionId);
+
+    @PUT(prefix + "/favorite/{solutionId}")
+    Call<Boolean> toggleFavorite(@Path("solutionId") Long solutionId);
 }
