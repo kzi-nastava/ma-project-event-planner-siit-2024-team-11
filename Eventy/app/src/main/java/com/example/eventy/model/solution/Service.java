06c4ecab4ca3b008eae4fb39ae6db12af211e1cc
Matijas Levang <micolevang@gmail.com>
2025-02-06 21:19:03 +0100

    [IMPROVEMENT] Sync DTO with backend DTO

--- Eventy/app/src/main/java/com/example/eventy/model/solution/Service.java
+++ Eventy/app/src/main/java/com/example/eventy/model/solution/Service.java
@@ -3,30 +3,137 @@ package com.example.eventy.model.solution;
 import com.example.eventy.model.enums.ReservationConfirmationType;
 import com.example.eventy.events.model.EventType;
 import com.example.eventy.solutions.model.Category;
+import com.example.eventy.solutions.model.CategoryWithID;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
-public class Service extends Solution {
+public class Service {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer discount;
+    private List<String> imageUrls;
+    @JsonProperty("isVisible")
+    private Boolean isVisible;
+    @JsonProperty("isAvailable")
+    private Boolean isAvailable;
+    private Category category;
+    private Collection<EventType> relatedEventTypes;
     private String specifics;
     private Integer minReservationTime;
     private Integer maxReservationTime;
     private Integer reservationDeadline;
     private Integer cancellationDeadline;
-    private ReservationConfirmationType reservationConfirmationType;
+    @JsonProperty("automaticReservationAcceptance")
+    private Boolean automaticReservationAcceptance;
 
     public Service() {
 
     }
 
-    public Service(String name, Category category, String description, double price, Integer discount, ArrayList<String> imageUrls, Boolean isDeleted, Boolean isVisible, Boolean isAvailable, String specifics, Integer minReservationTime, Integer maxReservationTime, Integer reservationDeadline, Integer cancellationDeadline, ReservationConfirmationType reservationConfirmationType, ArrayList<EventType> eventTypes) {
-        super(name, category, description, price, discount, imageUrls, isDeleted, isVisible, isAvailable, eventTypes);
+    public Service(long id, String name, String description, double price, int discount, List<String> imageUrls, boolean isVisible, boolean isAvailable, Category category, Collection<EventType> relatedEventTypes, String specifics, int minReservationTime, int maxReservationTime, int reservationDeadline, int cancellationDeadline, boolean automaticReservationAcceptance) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.discount = discount;
+        this.imageUrls = imageUrls;
+        this.isVisible = isVisible;
+        this.isAvailable = isAvailable;
+        this.category = category;
+        this.relatedEventTypes = relatedEventTypes;
         this.specifics = specifics;
         this.minReservationTime = minReservationTime;
         this.maxReservationTime = maxReservationTime;
         this.reservationDeadline = reservationDeadline;
         this.cancellationDeadline = cancellationDeadline;
-        this.reservationConfirmationType = reservationConfirmationType;
+        this.automaticReservationAcceptance = automaticReservationAcceptance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+
+    public List<String> getImageUrls() {
+        return imageUrls;
+    }
+
+    public void setImageUrls(List<String> imageUrls) {
+        this.imageUrls = imageUrls;
+    }
+
+    public Boolean getVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(Boolean visible) {
+        isVisible = visible;
+    }
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Collection<EventType> getRelatedEventTypes() {
+        return relatedEventTypes;
+    }
+
+    public void setRelatedEventTypes(Collection<EventType> relatedEventTypes) {
+        this.relatedEventTypes = relatedEventTypes;
     }
 
     public String getSpecifics() {
@@ -69,52 +176,11 @@ public class Service extends Solution {
         this.cancellationDeadline = cancellationDeadline;
     }
 
-    public ReservationConfirmationType getReservationConfirmationType() {
-        return reservationConfirmationType;
+    public Boolean getAutomaticReservationAcceptance() {
+        return automaticReservationAcceptance;
     }
 
-    public void setReservationConfirmationType(ReservationConfirmationType reservationConfirmationType) {
-        this.reservationConfirmationType = reservationConfirmationType;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Service)) return false;
-        if (!super.equals(o)) return false;
-        Service service = (Service) o;
-        return Objects.equals(specifics, service.specifics) &&
-                Objects.equals(minReservationTime, service.minReservationTime) &&
-                Objects.equals(maxReservationTime, service.maxReservationTime) &&
-                Objects.equals(reservationDeadline, service.reservationDeadline) &&
-                Objects.equals(cancellationDeadline, service.cancellationDeadline) &&
-                reservationConfirmationType == service.reservationConfirmationType;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), specifics, minReservationTime, maxReservationTime,
-                reservationDeadline, cancellationDeadline, reservationConfirmationType);
-    }
-
-    @Override
-    public String toString() {
-        return "Service{" +
-                "name='" + getName() + '\'' +
-                ", category='" + getCategory() + '\'' +
-                ", description='" + getDescription() + '\'' +
-                ", price=" + getPrice() +
-                ", discount=" + getDiscount() +
-                ", imageUrls=" + getImageUrls() +
-                ", isDeleted=" + getDeleted() +
-                ", isVisible=" + getVisible() +
-                ", isAvailable=" + getAvailable() +
-                ", specifics='" + specifics + '\'' +
-                ", minReservationTime=" + minReservationTime +
-                ", maxReservationTime=" + maxReservationTime +
-                ", reservationDeadline=" + reservationDeadline +
-                ", cancellationDeadline=" + cancellationDeadline +
-                ", reservationConfirmationType=" + reservationConfirmationType +
-                '}';
+    public void setAutomaticReservationAcceptance(Boolean automaticReservationAcceptance) {
+        this.automaticReservationAcceptance = automaticReservationAcceptance;
     }
 }
