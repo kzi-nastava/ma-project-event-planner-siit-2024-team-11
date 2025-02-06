39d94fe7a778fd514f5e028681405e951e2bc33a
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-02-06 23:32:09 +0100

    [FEAT] Initial ProductCreation fragment layout and functionalities

--- Eventy/app/src/main/java/com/example/eventy/products/ProductCreationFragment.java
+++ Eventy/app/src/main/java/com/example/eventy/products/ProductCreationFragment.java
@@ -1,66 +1,247 @@
 package com.example.eventy.products;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.ClipData;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.example.eventy.R;
+import com.example.eventy.common.PictureHelperService;
+import com.example.eventy.custom.ErrorOkDialog;
+import com.example.eventy.databinding.FragmentProductCreationBinding;
+import com.example.eventy.events.model.EventTypeCard;
+import com.example.eventy.products.model.CreateProduct;
+import com.example.eventy.products.model.Product;
+import com.example.eventy.solutions.model.CategoryWithID;
+import com.example.eventy.users.register.CarouselAdapter;
+import com.example.eventy.utils.ClientUtils;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProductCreationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ProductCreationFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CarouselAdapter carouselAdapter;
+    private ActivityResultLauncher<Intent> imagePickerLauncher;
+    private ArrayList<String> images = new ArrayList<>();
+    private FragmentProductCreationBinding binding;
+    private Long selectedCategoryId;
+    private List<CategoryWithID> categories;
+    private List<EventTypeCard> eventTypes;
 
     public ProductCreationFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProductCreationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProductCreationFragment newInstance(String param1, String param2) {
-        ProductCreationFragment fragment = new ProductCreationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_creation, container, false);
+        binding = FragmentProductCreationBinding.inflate(inflater, container, false);
+
+        ViewPager2 viewPager = binding.productPhotos;
+
+        carouselAdapter = new CarouselAdapter(images);
+        viewPager.setAdapter(carouselAdapter);
+
+        imagePickerLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    ArrayList<String> newImages = new ArrayList<>();
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        if (result.getData().getClipData() != null) {
+                            ClipData clipData = result.getData().getClipData();
+                            for (int i = 0; i < clipData.getItemCount(); i++) {
+                                try {
+                                    Uri imageUri = clipData.getItemAt(i).getUri();
+                                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(
+                                            getActivity().getContentResolver(), imageUri);
+                                    newImages.add(PictureHelperService.bitmapToBase64(bitmap));
+                                }
+                                catch (Exception e) {
+                                    ErrorOkDialog errorOkDialog = new ErrorOkDialog(getActivity(), "Error", "Error while selecting the picture!");
+                                    errorOkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                                    errorOkDialog.show();
+                                }
+                            }
+                        } else if (result.getData().getData() != null) {
+                            try {
+                                Uri imageUri = result.getData().getData();
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(
+                                        getActivity().getContentResolver(), imageUri);
+                                newImages.add(PictureHelperService.bitmapToBase64(bitmap));
+                            }
+                            catch (Exception e) {
+                                ErrorOkDialog errorOkDialog = new ErrorOkDialog(getActivity(), "Error", "Error while selecting the picture!");
+                                errorOkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                                errorOkDialog.show();
+                            }
+                        }
+
+                        carouselAdapter.updateImages(newImages);
+                        images = newImages;
+                    }
+                }
+        );
+
+        binding.addProductPhotosButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);  // Allow multiple selection
+            imagePickerLauncher.launch(intent);
+        });
+
+        Call<List<CategoryWithID>> call = ClientUtils.categoryService.getActiveCategories();
+        call.enqueue(new Callback<List<CategoryWithID>>() {
+            @Override
+            public void onResponse(Call<List<CategoryWithID>> call, Response<List<CategoryWithID>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    categories = response.body();
+                    MaterialAutoCompleteTextView categoryAutoCompleteTextView = binding.categoryAutoCompleteTextView;
+                    ArrayAdapter<CategoryWithID> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_dropdown_item_1line, response.body());
+                    categoryAutoCompleteTextView.setAdapter(adapter);
+
+                    categoryAutoCompleteTextView.setOnItemClickListener((parent, view, position, id) -> {
+                        CategoryWithID selectedCard = (CategoryWithID) parent.getItemAtPosition(position);
+                        selectedCategoryId = selectedCard.getId();
+                    });
+
+                    categoryAutoCompleteTextView.setOnClickListener(v -> {
+                        if (!categoryAutoCompleteTextView.isPopupShowing()) {
+                            categoryAutoCompleteTextView.showDropDown();
+                        }
+                    });
+
+                } else {
+                    ErrorOkDialog errorOkDialog = new ErrorOkDialog(getActivity(), "Error", "Error loading event types!");
+                    errorOkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    errorOkDialog.show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<CategoryWithID>> call, Throwable t) {
+                ErrorOkDialog errorOkDialog = new ErrorOkDialog(getActivity(), "Error", "Network error!");
+                errorOkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                errorOkDialog.show();
+            }
+        });
+
+        ChipGroup chipGroup = binding.chipGroup;
+        Call<EventTypeCard[]> call2 = ClientUtils.eventTypeService.getActiveEventTypes();
+        call2.enqueue(new Callback<EventTypeCard[]>() {
+            @Override
+            public void onResponse(Call<EventTypeCard[]> call, Response<EventTypeCard[]> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    eventTypes.clear();
+                    for (EventTypeCard et : response.body()) {
+                        Chip chip = new Chip(getContext());
+                        chip.setText(et.getName()); // text
+                        chip.setCheckable(true);
+                        chip.setTag(et.getId());    // value
+                        chipGroup.addView(chip);
+                        eventTypes.add(et);
+                    }
+                } else {
+                    ErrorOkDialog errorOkDialog = new ErrorOkDialog(getActivity(), "Error", "Error loading event types!");
+                    errorOkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    errorOkDialog.show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<EventTypeCard[]> call, Throwable t) {
+                ErrorOkDialog errorOkDialog = new ErrorOkDialog(getActivity(), "Error", "Network error!");
+                errorOkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                errorOkDialog.show();
+            }
+        });
+
+        binding.submitButton.setOnClickListener(v -> {
+            submit();
+        });
+
+        return binding.getRoot();
+    }
+
+    private void submit() {
+        CreateProduct newProduct = new CreateProduct();
+
+        newProduct.setName(binding.productNameInput.getText().toString());
+        newProduct.setDescription(binding.productDescriptionInput.getText().toString());
+        newProduct.setCategory(categories.stream()
+                .filter(category -> category.getId().equals(selectedCategoryId))
+                .findFirst().orElse(null));
+        newProduct.setPrice(Double.parseDouble(binding.productPriceInput.getText().toString()));
+        newProduct.setDiscount(Integer.parseInt(binding.productDiscountInput.getText().toString()));
+        newProduct.setIsAvailable(binding.isAvailable.isChecked());
+        newProduct.setIsVisible(binding.isVisible.isChecked());
+
+        List<Long> selectedEventTypeIds = new ArrayList<>();
+        for (int i = 0; i < binding.chipGroup.getChildCount(); i++) {
+            Chip chip = (Chip) binding.chipGroup.getChildAt(i);
+            if (chip.isChecked()) {
+                selectedEventTypeIds.add((Long) chip.getTag());
+            }
+        }
+        newProduct.setRelatedEventTypes(selectedEventTypeIds.stream().map(typeId -> eventTypes.stream().filter(type -> type.getId().equals(typeId)).findFirst().orElse(null)).collect(Collectors.toList()));
+        newProduct.setImageUrls(images);
+
+        Call<Product> call = ClientUtils.productService.create(newProduct);
+        call.enqueue(new Callback<Product>() {
+            @Override
+            public void onResponse(Call<Product> call, Response<Product> response) {
+                if (response.isSuccessful()) {
+                    Bundle args = new Bundle();
+                    args.putLong("solutionId", response.body().getId());
+
+                    NavController navController = Navigation.findNavController(getView());
+                    navController.popBackStack();
+                    navController.navigate(R.id.nav_solution_details, args);
+                } else {
+                    ErrorOkDialog errorOkDialog = new ErrorOkDialog(getActivity(), "Error", "Could not create the product!");
+                    errorOkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    errorOkDialog.show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Product> call, Throwable t) {
+                ErrorOkDialog errorOkDialog = new ErrorOkDialog(getActivity(), "Error", "Network error!");
+                errorOkDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                errorOkDialog.show();
+            }
+        });
     }
 }
