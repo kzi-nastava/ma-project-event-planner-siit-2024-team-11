c3bddf6c173198940e7041228086936c144b1a93
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-08-03 22:02:09 +0200

    Added conditional navigation, now we only show options that should be available to the user with the appropriate role

--- Eventy/app/src/main/java/com/example/eventy/users/services/LoggedInHelperService.java
+++ Eventy/app/src/main/java/com/example/eventy/users/services/LoggedInHelperService.java
@@ -47,51 +47,43 @@ public class LoggedInHelperService {
         return null;
     }
 
-    public static void manageNavigationItems() {LoggedInHelperService.mainActivity.supportInvalidateOptionsMenu();
-        /*
+    public static void manageNavigationItems() {
+        LoggedInHelperService.mainActivity.supportInvalidateOptionsMenu();
         String role = LoggedInHelperService.getRole();
         Menu menu = LoggedInHelperService.navigationView.getMenu();
-        */
 
-//        menu.findItem(R.id.nav_login).setVisible(false);
-//        menu.findItem(R.id.nav_register).setVisible(false);
-//        menu.findItem(R.id.nav_add_service).setVisible(false);
-//        menu.findItem(R.id.nav_edit_service).setVisible(false);
-//        menu.findItem(R.id.nav_event_organization).setVisible(false);
-//        menu.findItem(R.id.nav_own_services_test).setVisible(false);
-//        menu.findItem(R.id.nav_event_types).setVisible(false);
-//        menu.findItem(R.id.service_reservation).setVisible(false);
-//        menu.findItem(R.id.fast_registration).setVisible(false);
-//        menu.findItem(R.id.upgrade_profile).setVisible(false);
-//        menu.findItem(R.id.nav_category_management).setVisible(false);
-//        menu.findItem(R.id.nav_event_stats).setVisible(false);
-//        menu.findItem(R.id.nav_create_product).setVisible(false);
-//        menu.findItem(R.id.nav_pending_reviews).setVisible(false);
-//        menu.findItem(R.id.nav_pending_reports).setVisible(false);
+        menu.findItem(R.id.nav_login).setVisible(false);
+        menu.findItem(R.id.nav_register).setVisible(false);
+        menu.findItem(R.id.nav_manipulate_service).setVisible(false);
+        menu.findItem(R.id.nav_event_organization).setVisible(false);
+        menu.findItem(R.id.nav_own_services_test).setVisible(false);
+        menu.findItem(R.id.nav_event_types).setVisible(false);
+        menu.findItem(R.id.nav_category_home).setVisible(false);
+        menu.findItem(R.id.nav_event_stats).setVisible(false);
+        menu.findItem(R.id.nav_product_creation).setVisible(false);
+        menu.findItem(R.id.nav_pending_reviews).setVisible(false);
+        menu.findItem(R.id.nav_pending_reports).setVisible(false);
+        menu.findItem(R.id.nav_pricelist).setVisible(false);
 
-//        // Example logic: Show/Hide items based on role
-//        if ("ROLE_Admin".equals(role)) {
-//            menu.findItem(R.id.nav_event_types).setVisible(true);
-//            menu.findItem(R.id.nav_category_management).setVisible(true);
-//            menu.findItem(R.id.nav_event_stats).setVisible(false);
-//            menu.findItem(R.id.nav_pending_reviews).setVisible(true);
-//            menu.findItem(R.id.nav_pending_reports).setVisible(true);
-//        } else if ("ROLE_Organizer".equals(role)){
-//            menu.findItem(R.id.nav_event_organization).setVisible(true);
-//            menu.findItem(R.id.service_reservation).setVisible(true);
-//            menu.findItem(R.id.nav_event_stats).setVisible(false);
-//        } else if ("ROLE_Provider".equals(role)) {
-//            menu.findItem(R.id.nav_add_service).setVisible(true);
-//            menu.findItem(R.id.nav_edit_service).setVisible(true);
-//            menu.findItem(R.id.nav_own_services_test).setVisible(true);
-//            menu.findItem(R.id.nav_create_product).setVisible(true);
-//        } else if ("ROLE_AuthenticatedUser".equals(role)) {
-//            menu.findItem(R.id.upgrade_profile).setVisible(true);
-//        } else if (role == null) {
-//            menu.findItem(R.id.nav_login).setVisible(true);
-//            menu.findItem(R.id.nav_register).setVisible(true);
-//            menu.findItem(R.id.fast_registration).setVisible(true);
-//        }
-//
+        if ("ROLE_Admin".equals(role)) {
+            menu.findItem(R.id.nav_event_types).setVisible(true);
+            menu.findItem(R.id.nav_category_home).setVisible(true);
+            menu.findItem(R.id.nav_event_stats).setVisible(true);
+            menu.findItem(R.id.nav_pending_reviews).setVisible(true);
+            menu.findItem(R.id.nav_pending_reports).setVisible(true);
+        } else if ("ROLE_Organizer".equals(role)){
+            menu.findItem(R.id.nav_event_organization).setVisible(true);
+            menu.findItem(R.id.nav_event_stats).setVisible(true);
+        } else if ("ROLE_Provider".equals(role)) {
+            menu.findItem(R.id.nav_manipulate_service).setVisible(true);
+            menu.findItem(R.id.nav_own_services_test).setVisible(true);
+            menu.findItem(R.id.nav_product_creation).setVisible(true);
+            menu.findItem(R.id.nav_pricelist).setVisible(true);
+        } else if ("ROLE_AuthenticatedUser".equals(role)) {
+            menu.findItem(R.id.upgrade_profile).setVisible(true);
+        } else if (role == null) {
+            menu.findItem(R.id.nav_login).setVisible(true);
+            menu.findItem(R.id.nav_register).setVisible(true);
+        }
     }
 }
