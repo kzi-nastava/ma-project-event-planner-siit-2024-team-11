aaa8ee16a5a5859a013d0bce21c7a3e4a30f26b3
Matijas Levang <micolevang@gmail.com>
2025-08-03 15:21:17 +0200

    [FIX] Cancel button not working on PricelistItemEditDialog

--- Eventy/app/src/main/java/com/example/eventy/solutions/PricelistItemEditDialog.java
+++ Eventy/app/src/main/java/com/example/eventy/solutions/PricelistItemEditDialog.java
@@ -106,5 +106,9 @@ public class PricelistItemEditDialog extends Dialog {
             }
 
         });
+
+        cancelButton.setOnClickListener(v -> {
+            dismiss();
+        });
     }
 }
